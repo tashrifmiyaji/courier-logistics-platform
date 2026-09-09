192fac8bdcf3acf2089094af1f65c710dd51836a
tashrif-miyaji <tashrifmiyaji@gmail.com>
2026-09-09 20:20:44 +0600

    Add authentication middleware for role-based access control

---
+++ src/app/middleware/checkAuth.ts
@@ -0,0 +1,93 @@
+import type { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import type { JwtPayload } from "jsonwebtoken";
+import type { Role } from "../../generated/prisma/enums";
+import config from "../config";
+import { prisma } from "../lib/prisma";
+import { catchAsync } from "../utils/catchAsync";
+import { AppError } from "../utils/AppError";
+import { jwtUtils } from "../utils/jwt";
+
+export interface RequestUser {
+	email: string;
+	name: string;
+	userId: string;
+	role: Role;
+}
+
+declare global {
+	namespace Express {
+		interface Request {
+			user?: RequestUser;
+		}
+	}
+}
+
+// auth(Role.ADMIN, Role.USER, Role.Author)
+// auth() => ...requiredRoles => [Role.ADMIN, Role.USER, Role.AUTHOR]
+export const auth = (...requiredRoles: Role[]) => {
+	return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+		const token = req.cookies.accessToken
+			? req.cookies.accessToken
+			: req.headers.authorization?.startsWith("Bearer ")
+				? req.headers.authorization?.split(" ")[1]
+				: req.headers.authorization;
+
+		if (!token) {
+			throw new AppError(
+				httpStatus.UNAUTHORIZED,
+				"You are not logged in. Please log in to access this resource.",
+			);
+		}
+
+		const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
+
+		if (!verifiedToken.success) {
+			throw new AppError(
+				httpStatus.UNAUTHORIZED,
+				verifiedToken.error || "Invalid or expired token",
+			);
+		}
+
+		const { email, name, userId, role } = verifiedToken.data as JwtPayload;
+
+		if (requiredRoles.length && !requiredRoles.includes(role)) {
+			throw new AppError(
+				httpStatus.FORBIDDEN,
+				"Forbidden. You don't have permission to access this resource.",
+			);
+		}
+
+		const user = await prisma.user.findUnique({
+			where: {
+				id: userId,
+				email,
+				name,
+				role,
+			},
+		});
+
+		if (!user) {
+			throw new AppError(
+				httpStatus.NOT_FOUND,
+				"User not found. Please log in again.",
+			);
+		}
+
+		if (!user.isVerified) {
+			throw new AppError(
+				httpStatus.FORBIDDEN,
+				"Your are not verified!",
+			);
+		}
+
+		req.user = {
+			email,
+			name,
+			userId,
+			role,
+		};
+
+		next();
+	});
+};
