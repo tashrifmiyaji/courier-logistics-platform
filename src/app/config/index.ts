8ee98d20031c05548d2de2c3b21ceba300a74f81
tashrif-miyaji <tashrifmiyaji@gmail.com>
2026-09-09 20:02:06 +0600

    Add configuration file to load environment variables for the application

---
+++ src/app/config/index.ts
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+	node_env: process.env.NODE_ENV,
+	port: process.env.PORT,
+	database_url: process.env.DATABASE_URL,
+	bak_url: process.env.APP_URL,
+	frontend_url: process.env.FRONTEND_URL,
+	bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+	jwt_access_secret: process.env.JWT_ACCESS_SECRET!,
+	jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
+	jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
+	jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!,
+	google_client_id: process.env.GOOGLE_CLIENT_ID!,
+	admin_name: process.env.SUPER_ADMIN_NAME!,
+	admin_email: process.env.SUPER_ADMIN_EMAIL!,
+	admin_password: process.env.SUPER_ADMIN_PASSWORD!,
+	redis_user: process.env.REDIS_USER!,
+	redis_password: process.env.REDIS_PASSWORD!,
+	redis_host: process.env.REDIS_HOST!,
+	redis_port: process.env.REDIS_PORT!,
+	smtp_user: process.env.SMTP_USER!,
+	smtp_password: process.env.SMTP_PASSWORD!,
+	email_sender: process.env.EMAIL_SENDER!,
+	cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+	cloudinary_api_key: process.env.CLOUDINARY_API_KEY!,
+	cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET!,
+	bkash_base_url: process.env.BKASH_BASE_URL!,
+	bkash_username: process.env.BKASH_USERNAME!,
+	bkash_password: process.env.BKASH_PASSWORD!,
+	bkash_app_key: process.env.BKASH_APP_KEY!,
+	bkash_app_secret: process.env.BKASH_APP_SECRET!,
+	bkash_callback_url: process.env.BKASH_CALLBACK_URL!,
+};
